8094b0f84a8be7370d8385fa4b51b0591d50b2ac
Diogenes Fernandes <diofeher@gmail.com>
2024-10-30 13:24:40 -0300

    Backend: User testing for authentication

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -62,9 +62,6 @@ async def get_current_user(
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    import pdb
-
-    pdb.set_trace()
     # if current_user.disabled:
     # raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
